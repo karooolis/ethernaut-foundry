7aca9015e12b3a99f489cfa20d542a771f37091b
karooolis <hello@karolisram.com>
2023-04-10 13:59:19 +0300

    adjust NaughtCoin CTF numbering

--- test/15-NaughtCoin.t.sol
+++ test/15-NaughtCoin.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/14-NaughtCoin.sol";
+import "../src/15-NaughtCoin.sol";
 
 contract NaughtCoinTest is Test {
     NaughtCoin public naughtCoinCTF;
