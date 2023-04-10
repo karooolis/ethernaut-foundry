ce1d1c3c28da2b5722a24c2c9391be1d72fe6fd6
karooolis <hello@karolisram.com>
2023-04-10 13:41:20 +0300

    add NaughtCoin CTF

--- test/14-NaughtCoin.t.sol
+++ test/14-NaughtCoin.t.sol
@@ -1,17 +1,17 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import "forge-std/Test.sol";
-// import "../src/14-NaughtCoin.sol";
+import "forge-std/Test.sol";
+import "../src/14-NaughtCoin.sol";
 
-// contract NaughtCoinTest is Test {
-//     NaughtCoin public naughtCoinCTF;
+contract NaughtCoinTest is Test {
+    NaughtCoin public naughtCoinCTF;
 
-//     function setUp() public {
-//         naughtCoinCTF = new NaughtCoin();
-//     }
+    function setUp() public {
+        naughtCoinCTF = new NaughtCoin();
+    }
 
-//     function testAttack() public {
+    function testAttack() public {
 
-//     }
-// }
+    }
+}
